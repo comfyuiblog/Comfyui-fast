fd230777b51a0afbdf48a35302f07a14bcb86b8e
comfyuiblog <23scienceinsights@gmail.com>
2024-10-11 23:51:55 +0530

    Update temp.py

--- temp/temp.py
+++ temp/temp.py
@@ -21,32 +21,30 @@ class TextInputNode:
         return (user_text,)
 
 
+
 class PreviewVideoNode:
+
     @classmethod
     def INPUT_TYPES(s):
         return {
             "required": {
-                "video": ("VIDEO",), 
+                "video": ("VIDEO",),
             }
         }
 
-    CATEGORY = "Comfyuiblog"  
-    DESCRIPTION = "hello world!"  
+    CATEGORY = "Comfyuiblog"
+    DESCRIPTION = "hello world!"
 
-    RETURN_TYPES = () 
-    OUTPUT_NODE = True  
+    RETURN_TYPES = ()
 
-    FUNCTION = "load_video"  
+    OUTPUT_NODE = True
+
+    FUNCTION = "load_video"
 
     def load_video(self, video):
- 
         video_name = os.path.basename(video)
         video_path_name = os.path.basename(os.path.dirname(video))
-        
-    
-        print(f"Processing video: {video_name} in directory: {video_path_name}")
-        
-     
+        return {"ui":{"video":[video_name,video_path_name]}}
 
 NODE_CLASS_MAPPINGS = {
     "Text to Prompt": TextInputNode,
